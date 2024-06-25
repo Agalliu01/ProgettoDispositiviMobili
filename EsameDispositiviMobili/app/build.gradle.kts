e8d6ceb5ff5d00ed165624f904fc2979f37aac0b
Agalliu01 <tagalliu1@studenti.uninsubria.it>
2024-06-25 13:26:08 +0200

    Create prime activity. Ho collegato autenticazione e cloud di firebase mettendo le dipendenze necessarie. Inoltre, ho inserito su manifest le richieste per lettura media dal dispositivo per far caricare successivamente a utente le immagini per i post

--- EsameDispositiviMobili/app/build.gradle.kts
+++ EsameDispositiviMobili/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,7 +38,35 @@ android {
 
 dependencies {
 
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation ("com.google.android.material:material:1.12.0")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+
+    implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation("com.google.firebase:firebase-storage:21.0.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("com.google.firebase:firebase-database:21.0.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
+
+    implementation ("androidx.recyclerview:recyclerview:1.3.2")
+
+    implementation ("androidx.appcompat:appcompat:1.7.0")
+    implementation ("androidx.fragment:fragment-ktx:1.8.0")
+
+
+    implementation ("com.google.firebase:firebase-auth-ktx:23.0.0")
+    implementation ("com.google.firebase:firebase-firestore-ktx:25.0.0")
+    implementation ("com.google.firebase:firebase-storage-ktx:21.0.0")
+    implementation ("com.google.firebase:firebase-storage-ktx")
+    implementation ("com.google.firebase:firebase-firestore-ktx")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.firebase:firebase-analytics")
+
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
